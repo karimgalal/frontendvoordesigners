c6d1d425a84ae702d1dbeb9959b564ab404ba936
Karim <karim.galal@hva.nl>
2019-05-28 11:48:28 +0200

    Data laden

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -1,19 +1,42 @@
 var uri = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
+var main = document.querySelector("main"); //Dit is de main van de HTML
 
-var section = document.querySelector('section');
-console.log("section",section);
-
-
-console.log("loadimagesmetXHR");
 var request = new XMLHttpRequest();
 request.open('get', uri);
 request.responseType = 'json';
 request.send();
 
 request.addEventListener("load", function(){
-  var data = request.response;
   console.log("request is geladen: ",request.response);
-  //er is data
-  //nu kun je iets doen
-  section.textContent = JSON.stringify(data);
+
+  var data = request.response;
+  for(var i = 0; i < data.length; i++)
+  {
+    var film = data[i];
+
+    var section = document.createElement("section"); // Voor elke film is er een eigen section waar data in komt
+
+
+    var titel = document.createElement("h2");
+    titel.textContent = film.title;
+    section.appendChild(titel);
+
+    if(film.title == "Evil Dead"){
+      section.setAttribute("class", "red");
+    }
+
+    var simplePlot = document.createElement("p");
+    simplePlot.textContent = film.simple_plot;
+    section.appendChild(simplePlot);
+
+    var cover = document.createElement("img")
+    // cover.src = film.cover;
+    cover.setAttribute("src", film.cover);
+    section.appendChild(cover);
+
+
+    // section.classList.add("test");
+
+    main.appendChild(section);
+  }
 });
